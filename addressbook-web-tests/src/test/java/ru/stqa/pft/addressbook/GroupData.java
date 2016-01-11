3ae3605ae9b47d4f5176d821f0934aaab116f12b
Alexei Barantsev <barancev@gmail.com>
2016-01-11 19:46:59 +0300

    Реализован тест для создания групп, выделены вспомогательные методы и классы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupData {
+  private final String name;
+  private final String header;
+  private final String footer;
+
+  public GroupData(String name, String header, String footer) {
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getHeader() {
+    return header;
+  }
+
+  public String getFooter() {
+    return footer;
+  }
+}
