15ae04e48ab3f99a9d9cbdfffd1b1fa0420d8aab
Alexei Barantsev <barancev@gmail.com>
2016-02-06 15:06:33 +0300

    Добавлены примеры использования массивов и списков

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+  public static void main(String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (String l : languages) {
+      System.out.println("Я хочу выучить " + l);
+    }
+  }
+}
