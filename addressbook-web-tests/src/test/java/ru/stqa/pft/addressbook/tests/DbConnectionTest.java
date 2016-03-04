299ca9f4e3ab441fd3c07a918b108bb10372335f
Alexei Barantsev <barancev@gmail.com>
2016-03-04 16:12:05 +0300

    Реализовано извлечение информации о группах из базы данных

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+  @Test(dataProvider = "test")
+  public void testDbConnection() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
+      Groups groups = new Groups();
+      while (rs.next()) {
+        groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+
+      System.out.println(groups);
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
