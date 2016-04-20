9332497d5e9d0ee60b8e139f8087a2481ee3a486
Alexei Barantsev <barancev@gmail.com>
2016-04-20 11:54:59 +0300

    Тест исправлен

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -11,6 +11,6 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(5);
-    Assert.assertEquals(s.area(), 20.0);
+    Assert.assertEquals(s.area(), 25.0);
   }
 }
